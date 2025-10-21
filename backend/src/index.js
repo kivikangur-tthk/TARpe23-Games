9e0f24c3b5e05d3004c41ced151bbbc00e21c2b7
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2025-10-21 16:29:35 +0300

    Add .env files to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import http from 'http';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -8,10 +10,10 @@ app.get('/', (req, res) => {
     res.status(200).type('text/plain').send('Hello, World!');
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 httpServer.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}/`);
+    console.log(`Server is running at ${process.env.SERVER_URL}:${PORT}/`);
 });
 
 export { httpServer, app };
