3a1e3debb6268b4e5aab63b22d495a40552df58f
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2025-10-23 12:17:23 +0300

    Add game details endpoint and error handling

--- backend/src/data/gameService.js
+++ backend/src/data/gameService.js
@@ -1,7 +1,9 @@
 import Games from "./GameModel.js";
 export const gameService = {
     getGame: async (gameId) => {
-        const game = await Games.findByPk(gameId);
+        const game = await Games.findByPk(gameId,{
+            attributes: { exclude: ['createdAt', 'updatedAt'] },
+        });
         return game ? game.get({ plain: true }) : undefined;
     },
     getGames: async () => {
